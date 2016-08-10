a30535a4f34297ef5e38d75c08400369deec3151
VitaliyS <biggone24@gmail.com>
2016-08-10 18:28:20 +0300

    gghj

--- src/main/java/HW1/User.java
+++ src/main/java/HW1/User.java
@@ -4,11 +4,54 @@ package HW1;
  * Created by VitaliyS on 08/09/2016.
  */
 public class User {
-    public String name = "Chert";
-    public String rasa = "Niger";
-    public String color = "Chernuj";
-    public static void main( String[] args )
-    {
-       // System.out.println( "Hello World!" );
+
+    private String name;
+    private String rasa;
+    private int vozrast;
+
+    public User() {
+        this.name = "user1";
+        this.rasa = "demon";
+        this.vozrast = 0;
     }
+
+    public User(String name, String rasa, int vozrast) {
+        this.name = name;
+        this.rasa = rasa;
+        this.vozrast = vozrast;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public int getVozrast() {
+        return vozrast;
+    }
+
+    public void setVozrast(int vozrast) {
+        this.vozrast = vozrast;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", rasa='" + rasa + '\'' +
+                ", vozrast=" + vozrast +
+                '}';
+    }
+
 }
--- src/test/java/HW1/UserTest.java
+++ src/test/java/HW1/UserTest.java
@@ -1,5 +1,8 @@
 package HW1;
 
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,15 +10,28 @@ import org.testng.annotations.Test;
  * Created by VitaliyS on 08/09/2016.
  */
 public class UserTest {
-    @Test
-    public void User(){
-        User myUser = new User();
-        myUser.name = "Bitch";
-        myUser.color = "Black";
-        String rasa = myUser.rasa;
-String answer = "BigBitch";
-        Assert.assertEquals(answer, "BigBitch");
 
-         //System.out.println("Imya raba: " + myUser.name + " and " + "Cvet raba: " + myUser.color + ";" + " Rassa raba: " + myUser.rasa );
-    }
+    @Test
+   public void test() {
+        User user = new User();
+        User user2 = new User("Petya", "Russian", 99);
+        System.out.println(user.toString());
+        System.out.println(user2.toString());
+        user.setRasa("elf");
+        user.setVozrast(50);
+        System.out.println(user.toString());
+        Assert.assertEquals(user.getVozrast(), 51);
+   }
+
+//    @Test
+//    public User(){
+//        User myUser = new User();
+//        myUser.name = "Bitch";
+//        myUser.vozrast = 30;
+//        String rasa = myUser.rasa;
+//String answer = "BigBitch";
+//        Assert.assertEquals(answer, "BigBitch");
+//
+//         System.out.println("Imya raba: " + myUser.name + " and " + "Vozrast raba : " + myUser.vozrast + ";" + " Rassa raba: " + myUser.rasa );
+//    }
 }
